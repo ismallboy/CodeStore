ae904efdd25475b511130a115a57198f4b013da5
Allen <registeror@163.com>
2015-04-27 22:34:54 +0800

    alter the slq format file

--- 数据库/DB操作.sql
+++ 数据库/DB操作.sql
@@ -1,4 +1,20 @@
 --修改字段类型
 Alter table MSTB_MR_LOG alter column OPTION_STATUS_AFTER int null
 
+--增加字段
+USE [AmwayFrameworkWorkflow]
+GO
+
+/****** Object:  Table [dbo].[MSTB_MR_ROOM_INFO]    Script Date: 11/06/2014 20:19:43 ******/
+IF NOT EXISTS ( SELECT  d.name ,
+                    a.*
+            FROM    sys.syscolumns a
+                    INNER JOIN sysobjects d ON a.id = d.id
+                                               AND d.xtype = 'U'
+                                               AND d.name <> 'dtproperties'
+            WHERE   a.name = 'REMARK'
+                    AND d.name = 'MSTB_MR_ROOM_INFO' ) 
+    ALTER TABLE MSTB_MR_ROOM_INFO ADD REMARK NVARCHAR(500) NULL
+GO
+
 --
