f5d41d739b0deeb7cbed5ac6cfab1f021c59db97
ismallboy <registeror@163.com>
2016-02-02 10:41:15 +0800

    更改  ——

--- Code/C#/自定义/收集/Linq.cs
+++ Code/C#/自定义/收集/Linq.cs
@@ -5,3 +5,11 @@
 	{ 
 			msg += "座位号{0}存在重复值;\\n".FormatWith(string.Join(",",groupList));
 	}
+2.左连接
+	var q = from a in qCategory
+		join b in qUsed on a.ID equals b.CategoryID into usedNullList
+		from b in usedNullList.DefaultIfEmpty()
+		join c in qUseup on a.ID equals c.CategoryID into useupNullList
+		from c in useupNullList.DefaultIfEmpty()
+		select new { CategoryName = a.TicketCategory, TotalQuantity = a.Quantity, UsedQuantity = b == null ? 0 : b.UsedQuantity, UseupQuantity = c == null ? 0 : c.UseupQuantity };
+3.
--- Code/C#/自定义/收集/大杂烩.cs
+++ Code/C#/自定义/收集/大杂烩.cs
@@ -8,3 +8,7 @@
 3.asp.net webform 获取模板页控件
     ((TextBox)Master.FindControl("txtMsgTest")).Text;
 4.<%#Eval("AdjustScore","{0:N2}") %>
+
+5.校验最长文本：
+	//求字符串占用的字节数（中文2个字节，英文1一个字节，空格一个字节）
+	System.Text.Encoding.Default.GetBytes(txtTicketTitle.Text.Trim()).Length > 18
